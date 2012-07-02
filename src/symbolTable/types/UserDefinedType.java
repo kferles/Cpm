5efc696c39dd76a1bb157ca77711230d110bc24e
Kostas Ferles <kferles@gmail.com>
2012-07-02 22:35:48 +0300

    +ClassContentElement private class that describes a class element (access & storage specifiers) +AccessSpecifier enumeration (private, protected, public) +change insert* methods +equals method checks both name and the namespace that belongs +fix of subtype method (now searches in the superclasses set)

--- src/symbolTable/types/UserDefinedType.java
+++ src/symbolTable/types/UserDefinedType.java
@@ -1,39 +1,66 @@
 package symbolTable.types;
 
+import errorHandling.ErrorMessage;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import symbolTable.DefinesNamespace;
 
 /**
  *
  * @author kostas
  */
-public class UserDefinedType extends SimpleType {
-    
+public class UserDefinedType extends SimpleType implements DefinesNamespace{
+    //TODO: Decide error message, lines etc.
     HashSet<UserDefinedType> superClasses = null;
     
-    HashMap<String, Type> fields = null;
+    HashMap<String, ClassContentElement<Type>> fields = null;
     
-    HashMap<String, UserDefinedType> innerTypes = null;
+    HashMap<String, ClassContentElement<UserDefinedType>> innerTypes = null;
     
     /*
      * Methods are represented as a multi map to support method overloading.
      * That is, every name is maped to another map from a signature to a class Method
      * object. 
      */
-    HashMap<String, HashMap<Method.Signature, Method>> methods = null;
+    HashMap<String, HashMap<Method.Signature, ClassContentElement<Method>>> methods = null;
+    
+    DefinesNamespace belongsTo;
     
     boolean isAbstract;
+    
+    private class ClassContentElement <T>{
+        T element;
+        
+        AccessSpecifier access;
+        
+        boolean isStatic;
+        
+        public ClassContentElement(T element, AccessSpecifier access, boolean isStatic){
+            this.element = element;
+            this.access = access;
+            this.isStatic = isStatic;
+        }
+    }
+    
+    public enum AccessSpecifier{
+        Private, 
+        Protected,
+        Public
+    }
     
     /**
      * Constructs a UserDefinedType according to its name.
      * 
      * @param name Type's name.
      * @param isAbstract  whether the type is yet implemented or not.
+     * @param belongsTo   The namespace that the type belongs to (either a class or a namespace).
      */
-    public UserDefinedType(String name, boolean isAbstract){
+    public UserDefinedType(String name, DefinesNamespace belongsTo, boolean isAbstract){
         super(name);
         this.isAbstract = isAbstract;
+        this.belongsTo = belongsTo;
     }
     
     /**
@@ -43,8 +70,9 @@ public class UserDefinedType extends SimpleType {
      * checks must be performed before calling the constructor.
      * @param name          Type's name.
      * @param superTypes    All the super classes.
+     * @param belongsTo     The namespace that the type belongs to (either a class or a namespace).
      */
-    public UserDefinedType(String name, ArrayList<UserDefinedType> superTypes){
+    public UserDefinedType(String name, DefinesNamespace belongsTo, ArrayList<UserDefinedType> superTypes){
         super(name);
         superClasses = new HashSet<UserDefinedType>();
         for(UserDefinedType s : superTypes)
@@ -53,6 +81,7 @@ public class UserDefinedType extends SimpleType {
              * also handle methods, fields and inner types.
              */
             superClasses.add(s);
+        this.belongsTo = belongsTo;
     }
     
     /**
@@ -60,14 +89,14 @@ public class UserDefinedType extends SimpleType {
      * 
      * @param name  Field 's name.
      * @param t     A reference to the object that describes field 's type.
-     * @return      True if the field is unique inside the Type and false otherwise.
+     * @return      null if the field is unique inside the Type and an ErrorMessage otherwise.
      */
-    public boolean insertField(String name, Type t){
-        if(fields == null) fields = new HashMap<String, Type>();
+    public ErrorMessage insertField(String name, Type t, AccessSpecifier access, boolean isStatic){
+        if(fields == null) fields = new HashMap<String, ClassContentElement<Type>>();
         String key = name;      //think how to distinguish field that shadows fields of superclasses.
-        if(fields.containsKey(key)) return false;
-        fields.put(key, t);
-        return true;
+        if(fields.containsKey(key)) return new ErrorMessage("");
+        fields.put(key, new ClassContentElement<Type>(t, access, isStatic));
+        return null;
     }
     
     /**
@@ -75,23 +104,27 @@ public class UserDefinedType extends SimpleType {
      * 
      * @param name Type's name.
      * @param t    A reference to the UserDefinedType object that describes the Type.
-     * @return     True if this name is unique inside this scope.
+     * @return     null if this name is unique inside this scope.
      */
-    public boolean insertInnerType(String name, UserDefinedType t){
-        if(this.innerTypes == null) this.innerTypes = new HashMap<String, UserDefinedType>();
+    public ErrorMessage insertInnerType(String name, UserDefinedType t, AccessSpecifier access, boolean isStatic){
+        if(this.innerTypes == null) this.innerTypes = new HashMap<String, ClassContentElement<UserDefinedType>>();
+        if(name.equals(this.name) == true) return new ErrorMessage("");
         String key = name;
         if(innerTypes.containsKey(key)){
-            UserDefinedType t1 = innerTypes.get(key);
+            UserDefinedType t1 = innerTypes.get(key).element;
             if(t1.isAbstract == true){
-                innerTypes.put(key, t);
-                return true;
+                innerTypes.put(key, new ClassContentElement<UserDefinedType>(t, access, isStatic));
+                return null;
             }
             else if(t.isAbstract == false){
-                return false;
+                return new ErrorMessage("");
+            }
+            else{
+                return null; //check this one with an example.
             }
         }
-        innerTypes.put(key, t);
-        return true;
+        innerTypes.put(key, new ClassContentElement<UserDefinedType>(t, access, isStatic));
+        return null;
     }
     
     /**
@@ -99,22 +132,46 @@ public class UserDefinedType extends SimpleType {
      * 
      * @param name  Method's name.
      * @param m     A reference to the Method object that describes the method
-     * @return      True if method can be inserted and false if method cannot be overloaded.
+     * @return      null if method can be inserted and an ErrorMessage if method cannot be overloaded.
      */
-    public boolean insertMethod(String name, Method m){
-        if(this.methods == null) this.methods = new HashMap<String, HashMap<Method.Signature, Method>>();
+    public ErrorMessage insertMethod(String name, Method m, AccessSpecifier access, boolean isStatic){
+        //TODO: check for methods in super classes and if a virtual method is being overriden make it virtual in this class as well.
+        if(this.methods == null) this.methods = new HashMap<String, HashMap<Method.Signature, ClassContentElement<Method>>>();
         if(methods.containsKey(name)){
-            HashMap<Method.Signature, Method> m1 = methods.get(name);
-            if(m1.containsKey(m.s)) return false;
-            m1.put(m.s, m);
+            HashMap<Method.Signature, ClassContentElement<Method>> m1 = methods.get(name);
+            if(m1.containsKey(m.s)) return new ErrorMessage("");
+            m1.put(m.s, new ClassContentElement<Method>(m, access, isStatic));
         }
         else{
-            HashMap<Method.Signature, Method> m1 = new HashMap<Method.Signature, Method>();
-            m1.put(m.s, m);
+            HashMap<Method.Signature, ClassContentElement<Method>> m1 = new HashMap<Method.Signature, ClassContentElement<Method>>();
+            m1.put(m.s, new ClassContentElement<Method>(m, access, isStatic));
             methods.put(name, m1);
         }
         m.setType(this);
-        return true;
+        return null;
+    }
+    
+    @Override
+    public boolean equals(Object o){
+        if(!super.equals(o)) return false;
+        if(o instanceof UserDefinedType){
+            UserDefinedType ut = (UserDefinedType) o;
+            if(this.belongsTo == ut.belongsTo) return true;
+            return false;
+        }
+        else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + (this.superClasses != null ? this.superClasses.hashCode() : 0);
+        hash = 71 * hash + (this.fields != null ? this.fields.hashCode() : 0);
+        hash = 71 * hash + (this.innerTypes != null ? this.innerTypes.hashCode() : 0);
+        hash = 71 * hash + (this.methods != null ? this.methods.hashCode() : 0);
+        hash = 71 * hash + (this.belongsTo != null ? this.belongsTo.hashCode() : 0);
+        hash = 71 * hash + (this.isAbstract ? 1 : 0);
+        return hash + 71*super.hashCode();              //super's hashcode added for field name ...
     }
 
     @Override
@@ -122,9 +179,9 @@ public class UserDefinedType extends SimpleType {
         if(o == null) return false;
         if(o instanceof UserDefinedType){
             UserDefinedType ut = (UserDefinedType) o;
-            String name = ut.name;
-            if(innerTypes.containsKey(name)) return true;
-            for(UserDefinedType t : innerTypes.values()){
+            if(superClasses.contains(ut) || ut.equals(this)) return true;
+            for(Iterator<UserDefinedType> it = superClasses.iterator() ; it.hasNext() ; ){
+                UserDefinedType t = it.next();
                 if(this.subType(t)) return true;
             }
             return false;
