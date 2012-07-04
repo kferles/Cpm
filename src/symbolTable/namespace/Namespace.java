d31b22dfca179a345d0899bf0aee8ba4c473e775
Kostas Ferles <kferles@gmail.com>
2012-07-05 01:44:07 +0300

    Also add allSymbols Hash map as UserDefinedType class.

--- src/symbolTable/namespace/Namespace.java
+++ src/symbolTable/namespace/Namespace.java
@@ -2,6 +2,7 @@ package symbolTable.namespace;
 
 import errorHandling.ErrorMessage;
 import java.util.HashMap;
+import java.util.HashSet;
 import symbolTable.DefinesNamespace;
 import symbolTable.types.Method;
 import symbolTable.types.Type;
@@ -12,29 +13,47 @@ import symbolTable.types.Type;
  */
 public class Namespace implements DefinesNamespace{
     
+    HashMap<String, HashSet<Type>> allSymbols = new HashMap<String, HashSet<Type>>();
+    
     HashMap<String, Type> fields = null;
     
     HashMap<String, HashMap<Method.Signature, Method>> methods = null;
     
     HashMap<String, Namespace> innerNamespaces = null;
     
+    private void insertInAllSymbols(String name, Type entry){
+        HashSet<Type> set;
+        if(allSymbols.containsKey(name) == true){
+            set = allSymbols.get(name);
+        }
+        else{
+            set = new HashSet<Type>();
+            allSymbols.put(name, set);
+        }
+        set.add(entry);
+    }
+    
     public void insertField(String name, Type t) throws ErrorMessage{
         if(fields == null) fields = new HashMap<String, Type>();
-        if(fields.containsKey(name)) throw new ErrorMessage("");
+        if(allSymbols.containsKey(name) == true || innerNamespaces.containsKey(name)) throw new ErrorMessage("");
+        //if(fields.containsKey(name)) throw new ErrorMessage(""); //probably useless
         fields.put(name, t);
+        insertInAllSymbols(name, t);
     }
     
     public void insertMethod(String name, Method m) throws ErrorMessage{
         if(methods == null) methods = new HashMap<String, HashMap<Method.Signature, Method>>();
-        if(methods.containsKey(name)){
+        if(methods.containsKey(name) == true){
             HashMap<Method.Signature, Method> ms = methods.get(name);
             if(ms.containsKey(m.getSignature())) throw new ErrorMessage("");
             ms.put(m.getSignature(), m);
         }
         else{
+            if(allSymbols.containsKey(name) == true || innerNamespaces.containsKey(name) == true) throw new ErrorMessage("");
             HashMap<Method.Signature, Method> new_entry = new HashMap<Method.Signature, Method>();
             new_entry.put(m.getSignature(), m);
             methods.put(name, new_entry);
+            insertInAllSymbols(name, m);
         }
     }
     
