8c4409402e0e2f273a2ca15ed8cfe2152f289402
Kostas Ferles <kferles@gmail.com>
2012-07-06 01:15:58 +0300

    initial commit of methods prototypes.

--- src/symbolTable/DefinesNamespace.java
+++ src/symbolTable/DefinesNamespace.java
@@ -4,10 +4,22 @@
  */
 package symbolTable;
 
+import symbolTable.types.Method;
+import symbolTable.types.Type;
+
 /**
  *
  * @author kostas
  */
 public interface DefinesNamespace {
     
+    public Type findSymbol(String name);
+    
+    public Type findSymbol(String name, DefinesNamespace fromNamespace);
+    
+    public DefinesNamespace findNamespace(String name);
+    
+    public Method findMethod(String name, Method.Signature s);
+    
+    public Type findField(String name);
 }
