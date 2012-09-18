4ac99f950e8981214962d0dc05811a2213c91c3d
kostas <kostas@kostas-CQ2702EVm>
2012-09-18 14:56:42 +0300

    +override equals and hashcode methods +isComplete method

--- src/symbolTable/types/CpmArray.java
+++ src/symbolTable/types/CpmArray.java
@@ -5,9 +5,8 @@
 package symbolTable.types;
 
 import errorHandling.AmbiguousBaseClass;
-import java.util.ArrayList;
-import symbolTable.namespace.Namespace;
-import symbolTable.namespace.SynonymType;
+import errorHandling.VoidDeclaration;
+import symbolTable.namespace.CpmClass;
 
 /**
  *
@@ -37,6 +36,26 @@ public class CpmArray extends Type {
     }
     
     @Override
+    public boolean equals(Object o){
+        if(o == null) return false;
+        if(o instanceof CpmArray){
+            CpmArray ar = (CpmArray) o;
+            if(this.array_of.equals(ar.array_of) == false) return false;
+            if(this.dimensions_num != ar.dimensions_num) return false;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + (this.array_of != null ? this.array_of.hashCode() : 0);
+        hash = 67 * hash + this.dimensions_num;
+        return hash;
+    }
+    
+    @Override
     protected StringBuilder getString(StringBuilder aggr) {
         aggr.append(" ");
         for(int i = 0 ; i < this.dimensions_num ; ++i){
@@ -53,6 +72,11 @@ public class CpmArray extends Type {
     @Override
     public boolean subType(Type o) throws AmbiguousBaseClass {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public boolean isComplete(CpmClass current) throws VoidDeclaration{
+        return this.array_of.isComplete(current);
     }
     
 }
