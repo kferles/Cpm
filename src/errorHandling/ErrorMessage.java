b7dd6c8cdba0f8cbd37d975591e73531215a8fd6
kostas <kostas@kostas-CQ2702EVm>
2012-09-18 14:51:00 +0300

    Add static method getFullName for printing methods

--- src/errorHandling/ErrorMessage.java
+++ src/errorHandling/ErrorMessage.java
@@ -1,10 +1,19 @@
 package errorHandling;
 
+import symbolTable.types.Method;
+
 /**
  *
  * @author kostas
  */
 public class ErrorMessage extends Exception{
+    
+    public static String getFullName(Method m, String name){
+        String id = m.getParent().getName();
+        if(id.equals("") == false) id += "::";
+        id += name;
+        return id;
+    }
     
     public ErrorMessage(String msg){
         super(msg);
