4b392f046ec122bfc372967388e4236574d57b98
kostas <kostas@kostas-ThinkPad-T430u>
2013-02-07 23:56:15 +0200

    Name lookup and external definitions ...

--- src/errorHandling/NotDeclared.java
+++ src/errorHandling/NotDeclared.java
@@ -1,14 +1,22 @@
 package errorHandling;
 
 /**
- *
+ * Class for creating an error message when the parser tries to find a symbol
+ * that is not declared.
+ * 
  * @author kostas
  */
 public class NotDeclared extends ErrorMessage{
     
-    
+    /**
+     * Constructs an object given the name of the symbol, its line and the position in the line.
+     * 
+     * @param symbolName The identifier from the user input.
+     * @param line The line that contains the identifier.
+     * @param pos  And the position inside the line.
+     */
     public NotDeclared(String symbolName, int line, int pos){
-        super("line" + line + ":" + pos + " error: '" + symbolName +"' has not been declared");
+        super("line " + line + ":" + pos + " error: '" + symbolName +"' has not been declared");
     }
 
 
